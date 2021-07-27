7dec51de58ff4259588297932af27dc97c926452
Muhammad Abdur Rakib <abdur.rakib.1508@gmail.com>
2021-07-27 22:08:26 +0600

    created base structure of webpages

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import CustomUser, Profile
+from teachers.models import Teacher
+from students.models import Student
 
 
 @receiver(post_save, sender=CustomUser)
