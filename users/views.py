a980d0bc432eaaea7535e9950dfdea0ac8ceabc0
Muhammad Abdur Rakib <abdur.rakib.1508@gmail.com>
2021-07-28 20:32:46 +0600

    styling and fixing cards

--- users/views.py
+++ users/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from courses.models import Instructor, Enrollment
+
 
 def dashboard(request):
-    return render(request, 'users/index.html', {})
+    report_count = 0
+    course_count = Instructor.objects.filter(
+        teacher__teacher=request.user
+    ).count()
+    student_count = Enrollment.objects.filter(
+        course__teacher__teacher=request.user
+    ).count()
+    context = {
+        'report_count': report_count,
+        'course_count': course_count,
+        'student_count': student_count,
+    }
+    return render(request, 'users/index.html', context)
